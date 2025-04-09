fa883b53b59d2c245eba99cd4dab986f70d4455f
fidanveliyeva <veliyevafidan355@gmail.com>
2025-04-10 01:04:07 +0400

    blog updated

---
+++ assets/js/navbar.js
@@ -0,0 +1,8 @@
+function hamburg() {
+    document.querySelector('.dropdown').classList.add('active');
+  }
+  
+  function cancel() {
+    document.querySelector('.dropdown').classList.remove('active');
+  }
+  
