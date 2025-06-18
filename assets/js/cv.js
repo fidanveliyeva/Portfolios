cd935a71f1ce214c6b50281b85291c3d184e4310
fidanveliyeva <veliyevafidan355@gmail.com>
2025-06-18 21:53:04 +0400

    cv impoved

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const downloadButton = document.getElementById("downloadCV");
-    
-    downloadButton.addEventListener("click", function () {
-        const fileUrl = "./assets/cv/Fidan-Valiyeva-Resume.pdf" // Ensure the path is correct
-        const link = document.createElement("a");
-        link.href = fileUrl;
-        link.download = "Fidan-Valiyeva-Resume.pdf";  // File name for download
-        link.target = "_blank";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    });
+  const downloadButton = document.getElementById("downloadCV");
+
+  downloadButton.addEventListener("click", function () {
+    const fileUrl = "./assets/cv/Fidan-Valiyeva-Resume.pdf"; // Ensure the path is correct
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = "Fidan-Valiyeva-Resume.pdf"; // File name for download
+    link.target = "_blank";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
 });
